89e204f02cb4b5f31de6033032c8861cf12fa382
David Fisher <fisherds@rose-hulman.edu>
2023-09-11 13:28:32 -0400

    just before unit testing

--- learning_dart/lib/tic_tac_toe_game.dart
+++ learning_dart/lib/tic_tac_toe_game.dart
@@ -5,6 +5,33 @@ enum TicTacToeState { xTurn, oTurn, xWon, oWon, tie }
 class TicTacToeGame {
   final board = List<TicTacToeMark>.filled(9, TicTacToeMark.none);
   var state = TicTacToeState.xTurn;
+
+  String get stateString => state.toString().split(".").last;
+
+  String get boardString {
+    var bString = "";
+    for (final mark in board) {
+      // if (mark == TicTacToeMark.x) {
+      //   bString += "X";
+      // } else if (mark == TicTacToeMark.o) {
+      //   bString += "O";
+      // } else {
+      //   bString += "-";
+      // }
+      bString += (mark == TicTacToeMark.x)
+          ? "X"
+          : ((mark == TicTacToeMark.o) ? "O" : "-");
+    }
+    return bString;
+  }
+
+  // void pressedSquare({required int index}) {
+  void pressedSquare(int index) {}
+
+  @override
+  String toString() {
+    return "$stateString $boardString";
+  }
 }
 
 void main() {
@@ -16,7 +43,18 @@ void main() {
   // print(someState);
 
   var game = TicTacToeGame();
+  // game.board[0] = TicTacToeMark.x;
+  // game.board[4] = TicTacToeMark.x;
+  // game.board[2] = TicTacToeMark.o;
+  // game.board[7] = TicTacToeMark.o;
+  // game.state = TicTacToeState.tie;
   // print(game.state);
   // print(game.board);
+  print(game);
+  game.pressedSquare(0);
+  print(game);
+  game.pressedSquare(2);
+  print(game);
+  game.pressedSquare(4);
   print(game);
 }
