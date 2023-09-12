6184aa1e2477d3322b0fcb0357ff5b66a15f2d31
David Fisher <fisherds@rose-hulman.edu>
2023-09-12 13:14:35 -0400

    Finished TTT Game model object

--- learning_dart/lib/tic_tac_toe_game.dart
+++ learning_dart/lib/tic_tac_toe_game.dart
@@ -25,10 +25,15 @@ class TicTacToeGame {
     return bString;
   }
 
-  // void pressedSquare({required int index}) {
   void pressedSquare(int index) {
     // Check the index to be sure it it 0 to 8.
+    if (index < 0 || index > 8) {
+      return; // Do nothing.
+    }
     // Check if the board square is empty (return silent and do nothing if true)
+    if (board[index] != TicTacToeMark.none) {
+      return; // Do nothing.
+    }
     if (state == TicTacToeState.xTurn) {
       board[index] = TicTacToeMark.x;
       state = TicTacToeState.oTurn;
@@ -40,7 +45,32 @@ class TicTacToeGame {
     }
   }
 
-  void _checkForWin() {}
+  void _checkForWin() {
+    // Check for a tie.
+    if (!board.contains(TicTacToeMark.none)) {
+      state = TicTacToeState.tie;
+    }
+    final bString = boardString;
+    final linesOf3 = <String>[];
+    linesOf3.add(bString[0] + bString[1] + bString[2]);
+    linesOf3.add(bString[3] + bString[4] + bString[5]);
+    linesOf3.add(bString[6] + bString[7] + bString[8]);
+
+    linesOf3.add(bString[0] + bString[3] + bString[6]);
+    linesOf3.add(bString[1] + bString[4] + bString[7]);
+    linesOf3.add(bString[2] + bString[5] + bString[8]);
+
+    linesOf3.add(bString[0] + bString[4] + bString[8]);
+    linesOf3.add(bString[2] + bString[4] + bString[6]);
+    for (final lineOf3 in linesOf3) {
+      if (lineOf3 == "XXX") {
+        state = TicTacToeState.xWon;
+      }
+      if (lineOf3 == "OOO") {
+        state = TicTacToeState.oWon;
+      }
+    }
+  }
 
   @override
   String toString() {
